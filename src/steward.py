a6890b3baa805666fe1e79eba0e043864136383a
Sam Hart <hartsn@gmail.com>
2011-09-07 17:46:13 -0400

    start to the steward and daemon functionality

---
+++ src/steward.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env Python
+
+'''
+steward.py
+----------
+The steward is the main control daemon that acts as the communicator link
+between the client agent and CCMS.
+'''
+
+# vim:set ai et sts=4 sw=4 tw=80:
