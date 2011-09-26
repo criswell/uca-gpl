9d28fd5ff284adfb134a0e08e8303b432d92e712
Sam Hart <hartsn@gmail.com>
2011-09-26 15:46:41 -0400

    silly convenience script for updating the hosts file in windows

---
+++ poc/host-update-win32.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+''' Silly host munger '''
+
+f = open('C:\windows\system32\drivers\etc\hosts', 'a')
+
+f.write('10.4.0.29       goblinserver2')
+f.write('172.16.3.10     rmssrvr01.eil-infra.com rmssvr01')
+f.write('172.16.3.10     eilauto01.eil-infra.com eilauto01')
+
+f.close()
