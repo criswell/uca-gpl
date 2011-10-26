c789a9571f9e26e1410ec14e62c5c3c5ebf4640e
Sam Hart <hartsn@gmail.com>
2011-10-26 13:45:18 -0400

    flesh it out with something functional, but probably needs changing later

--- src/runtests.py
+++ src/runtests.py
@@ -1,3 +1,15 @@
 import sys, os
 
 sys.path.insert(0, os.path.dirname(__file__))
+
+ALL_TESTS =
+    [
+        'test_platform_id.py',
+    ]
+
+for test in ALL_TESTS:
+    # FIXME - surely there's a better way to do this
+    line = "python tests/%s" % test
+    print os.system(line)
+
+# vim:set ai et sts=4 sw=4 tw=80:
