b9f10454851acc5e293e17755d4f1967d5064a26
Sam Hart <hartsn@gmail.com>
2011-10-26 13:56:26 -0400

    test the other identifiers

--- src/tests/test_platform_id.py
+++ src/tests/test_platform_id.py
@@ -32,6 +32,35 @@ class TestPlatformID(unittest.TestCase):
 
         self.assertEqual(self.platformID.IS_LINUX, isLinux)
 
+    def test_SysID(self):
+        sysID = None
+        try:
+            stream = os.popen('/opt/intel/eil/clientagent/tools/clientagent-helper.sh --sysid')
+            output = stream.readlines()
+            stream.close()
+
+            if len(output) == 1:
+                sysID = output[0]
+        finally:
+            stream.close()
+
+        self.assertEqual(self.platformID.VARIANT.SYSID, sysID)
+
+    def test_Platform(self):
+        platform = None
+        try:
+            stream = os.popen('/opt/intel/eil/clientagent/tools/clientagent-helper.sh --platform')
+            output = stream.readlines()
+            stream.close()
+
+            if len(output) == 1:
+                platform = output[0]
+        finally:
+            stream.close()
+
+        self.assertEqual(self.platformID.VARIANT.PLATFORM, platform)
+
+
 if __name__ == '__main__':
     unittest.main()
 
