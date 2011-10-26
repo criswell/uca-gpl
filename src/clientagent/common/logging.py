18522528d77c583b97247ba4983b829cf998dce3
Sam Hart <hartsn@gmail.com>
2011-10-26 14:29:14 -0400

    logging to be a borgified class?

--- src/clientagent/common/logging.py
+++ src/clientagent/common/logging.py
@@ -5,6 +5,12 @@ logging.py
 The client agent's logging class. Simple, cross-platform, logging functionality.
 '''
 
-class Logging
+class Logging:
+    __borg_state = {}
+
+    __is_setup = False
+
+    def __init__(self):
+        self.__dict__ = self.__borg_state
 
 # vim:set ai et sts=4 sw=4 tw=80:
