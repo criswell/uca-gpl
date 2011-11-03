f4165b90395f1d8c972b275dec31454f3eefc66a
Sam Hart <hartsn@gmail.com>
2011-11-03 12:18:06 -0400

    need a common root for all future operations

--- src/clientagent/__init__.py
+++ src/clientagent/__init__.py
@@ -1,11 +1,13 @@
 import logging
 import os
+from clientagent.common.platform_id import PlatformID
 
 class ClientAgentState:
     '''
     Very basic state machine used by various client agent modules.
     '''
     INIT_SETUP = False
+    CLIENTAGENT_ROOT = ""
 
 if not ClientAgentState.INIT_SETUP:
     platformID = PlatformID()
@@ -15,10 +17,12 @@ if not ClientAgentState.INIT_SETUP:
     # from command-line and have the logging go to stdout
     if platformID.IS_WINDOWS:
         # FIXME - Will want this to be the same log we have used previously
-        logging.basicConfig(filename='C:\\eil\\clienagent.log')
+        CLIENTAGENT_ROOT = 'C:\\eil'
+        logging.basicConfig(filename='%s\\clienagent.log' % CLIENTAGENT_ROOT)
     else:
-        # Something intelligent for a default
-        fn = '/opt/intel/eil/clientagent/home/client-agent-base.log'
+        # Our root will be defined by previously issued LANANA/LSB requirements
+        CLIENTAGENT_ROOT = '/opt/intel/eil/clientagent/'
+        fn = '%s/home/client-agent-base.log' % CLIENTAGENT_ROOT
         try:
             stream = os.popen('/opt/intel/eil/clientagent/tools/clientagent-helper.sh --stdlog')
             output = stream.readlines()
--- src/clientagent/common/config.py
+++ src/clientagent/common/config.py
@@ -1,4 +1,6 @@
 import ConfigParser as configparser
+import clientagent
+from clientagent.common.platform_id import PlatformID
 
 class Config:
     '''
@@ -6,6 +8,7 @@ class Config:
     '''
 
     def __init__(self):
-        
+        self.platformID = PlatformID()
+        self.configFile = "%s/home/clientagent.cfg" % clientagent.ClientAgentState.CLIENTAGENT_ROOT
 
 # vim:set ai et sts=4 sw=4 tw=80:
