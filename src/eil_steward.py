6739c5a6ab047fdb60ddcbf5af3d6053f0698559
Sam Hart <hartsn@gmail.com>
2011-11-18 16:55:02 -0500

    stub out the steward handler

--- src/eil_steward.py
+++ src/eil_steward.py
@@ -18,5 +18,21 @@ else:
     # Linux
     from steward.libdaemon.unix_daemon import Daemon
 
+class StewardHandler(Daemon):
+
+    def local_init(self):
+        pass
+
+    def local_shutdown(self):
+        pass
+
+    def run(self):
+        pass
+
+def usage():
+    pass
+
+if __name__ == "__main__":
+    pass
 
 # vim:set ai et sts=4 sw=4 tw=80:
