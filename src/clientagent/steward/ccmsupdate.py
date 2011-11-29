67847a5c73a0178e831b28433b5e2c8efbd8c6bb
Sam Hart <hartsn@gmail.com>
2011-11-29 12:32:10 -0500

    stubbed ccms update operator

---
+++ src/clientagent/steward/ccmsupdate.py
@@ -0,0 +1,16 @@
+'''
+ccmsupdate.py
+-------------
+Main atomic operator class for the CCMS updates.
+'''
+
+from clientagent.steward.atom import Atom
+
+class CCMS_Update(Atom):
+    def __init__(self):
+        pass
+
+    def Update(self, timeDelta):
+        pass
+
+# vim:set ai et sts=4 sw=4 tw=80:
