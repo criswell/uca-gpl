b7a799cb9ef6cd580597692e0d16d937397563f5
Sam Hart <hartsn@gmail.com>
2012-01-03 13:32:35 -0500

    linux dispatcher script logic

--- src/clientagent/dispatcher_helper.py
+++ src/clientagent/dispatcher_helper.py
@@ -6,6 +6,7 @@ Any helper functions for the dispatcher that do not fit in the main module.
 
 import os
 import logging
+from clientagent import ClientAgentState
 
 # Linux specific methods
 
@@ -16,7 +17,46 @@ def linux_ExecuteCommand(command):
     dispatcher script to filter out incorrect commands.
 
     Will return the result.
+
+    @param command A string containing the command needed by the dispatcher
+    @returns True on success. False on failure.
     '''
-    pass
+    logger = logging.getLogger('linux_ExecuteCommand')
+    command_file = "%s/%s" % (ClientAgentState.COMDIR, command)
+
+    logger.debug('Placing command file %s' % command_file)
+
+    try:
+        f = open(command_file, 'w')
+        f.truncate()
+        # The current dispatcher specification under Linux is that the content
+        # of this file is meaningless. However, we do reserve the right to
+        # modify that spec in the future so we can pass command values.
+        f.write('foo')
+        f.close()
+
+        dispatcherPath = '%s/clientagent-dispatcher.sh' % ClientAgentState.BINDIR
+
+        logger.debug('Executing dispatcher %s' % dispatcherPath)
+
+        try:
+            # FIXME, do we need to use the script elevator here?
+            stream = os.popen(dispatcherPath)
+            output = stream.readlines()
+            stream.close()
+
+            # FIXME - Once we start parsing output from dispatcher, we should
+            # do something with output
+            return True
+        except:
+            logger.critical('Error executing dispatcher script')
+            return False
+        finally:
+            stream.close()
+    except:
+        logger.critical('Error touching command file')
+        return False
+    finally:
+        f.close()
 
 # vim:set ai et sts=4 sw=4 tw=80:
