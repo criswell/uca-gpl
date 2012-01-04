d45261d3bbc5e6473daed5b2151e0cc7c0deb7f6
Sam Hart <hartsn@gmail.com>
2012-01-04 12:22:26 -0500

    move join logic into handler

--- src/clientagent/steward/ccmsupdate.py
+++ src/clientagent/steward/ccmsupdate.py
@@ -12,6 +12,7 @@ from clientagent.common.utility import getIfInfo
 from clientagent import get_config
 from clientagent.dispatcher import Dispatcher
 from clientagent.steward.commandhandler import handleReboot
+from clientagent.steward.commandhandler import handleJoin
 
 # SUDs
 from suds.client import Client
@@ -216,97 +217,7 @@ class CCMS_Update(Atom):
                 elif commandName == 'reboot':
                     handleReboot(self, dispatcher, result)
                 elif commandName == 'join domain':
-                    doma = 'dl.inteleil.com'
-                    retry = 1
-
-                    if result.CommandParameters != None:
-                        nbrparms = len(result.CommandParameters.KeyValueOfstringstring)
-                        parmidx = 0
-                        while parmidx < nbrparms:
-                            pkey = result.CommandParameters.KeyValueOfstringstring[parmidx].Key
-                            pval = result.CommandParameters.KeyValueOfstringstring[parmidx].Value
-                            if pkey == "Domain Name":
-                                cmdDomain = pval
-                                parmidx = nbrparms + 1
-                            else:
-                                parmidx += 1
-
-                    joinExitCode = self.dispatcher.join(commandName, domain)
-                    ##      ******  in CCMS they are:     *******
-                    ##  COMMAND_ISSUED,             == 0
-                    ##  COMMAND_RECEIVED,           == 1
-                    ##  COMMAND_EXECUTION_STARTED,  == 2
-                    ##  COMMAND_EXECUTION_COMPLETE, == 3
-                    ##  COMMAND_FAILED,             == 4
-                    ##  WAIT_FOR_MANUAL_STEP,       == 5
-                    ##  COMMAND_TIMED_OUT,          == 6
-                    ##  COMMAND_DELAYED_RESPONSE,   == 7
-                    ##  COMMAND_RETRY               == 8
-
-                    ## Note: return codes of 0, 2691 (already joined), and AD
-                    ## acct create are handled by a bitwise setting on an input
-                    ## parm - for join only - with UNjoin you do not have the
-                    ## bitwise setting and must check each return
-                    ##                                        RC - ver.Nov 2011
-
-                    if joinExitCode == 0:
-                        rstat = 'COMMAND_EXECUTION_COMPLETE'
-                        rsuc = True
-                        rresult = 0
-                        rerr = result.ErrorCode
-                        rtime = result.ExpectedTimeOut
-                        rOID = result.OperationID
-                        rmt= result.SetMachineType
-                        cACK = generateCommand(self.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
-                    elif joinExitCode == 1355:
-                        # do nothing
-                        rstat = 'COMMAND_EXECUTION_COMPLETE'
-                        rsuc = True
-                        rresult = 0
-                        rerr = result.ErrorCode
-                        rtime = result.ExpectedTimeOut
-                        rOID = result.OperationID
-                        rmt= result.SetMachineType
-                        cACK = generateCommand(self.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
-                    elif joinExitCode == 1219:
-                        # Multiple sessions to a server- disconnect all previous
-                        # sessions and try again?? will need to add the RETRY
-                        # logic from Mahdu TAF client agent
-                        rstat = 'COMMAND_RETRY'
-                        rsuc = False
-                        rresult = 1219
-                        rerr = 1219
-                        rtime = result.ExpectedTimeOut
-                        rOID = result.OperationID
-                        rmt= result.SetMachineType
-                        cACK = generateCommand(self.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
-
-                    # Retry logic from win Client Agent version -
-                    # for Join Domain only
-                    if retry <= self.MAX_JOIN_RETRIES:
-                        retry =+ 1
-
-                        self.dispatcher.tcpDiag()     ## release renew IP first
-                        time.sleep(15)
-
-                        joinExitCode = self.dispatcher.join(cmdName, cmdDomain)
-                        RetryReturn(joinExitCode, rerr, rtime, rOID, rmt)
-                    else:
-                        rstat = 'COMMAND_FAILED'
-                        rsuc = False
-                        rresult = None
-                        rerr = 'domain join failed'
-                        rtime = result.ExpectedTimeOut
-                        rOID = result.OperationID
-                        rmt= result.SetMachineType
-                        cACK = generateCommand(self.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
-
-                    # All return codes will now reply back to CCMS via function
-                    # below using their own web request - not the original
-                    # getcommand request  RC - NOV 2011
-                    ACKresult = ACKclient.service.UpdateCommandStatus(ctx, cACK)
-
-                    self.logger.debug('domain join> CCMS return Comand Status Update Result: ' + ACKresult)
+                    handleJoin(self, result)
                 else:
                     # FIXME TODO
                     pass
--- src/clientagent/steward/commandhandler.py
+++ src/clientagent/steward/commandhandler.py
@@ -13,6 +13,7 @@ def handleReboot(ccmsUpdate, dispatcher, result):
     @param dispatcher The dispatcher instanct.
     @param result The result from the CCMS update call
     '''
+    commandName = result.CommandName
     rebcode = dispatcher.reboot('CCMS Reboot', 10)
     ccmsUpdate.ACKclient = ccmsUpdate.setStatusUpdateHeaders(ccmsUpdate.ACKclient, txID)
     if rebcode:
@@ -23,7 +24,7 @@ def handleReboot(ccmsUpdate, dispatcher, result):
         rtime = result.ExpectedTimeOut
         rOID = result.OperationID
         rmt= result.SetMachineType
-        cACK = ccmsUpdate.generateAckCommand(ccmsUpdate.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+        cACK = ccmsUpdate.generateAckCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
     else:
         rstat = 'COMMAND_FAILED'
         rsuc = False
@@ -32,8 +33,103 @@ def handleReboot(ccmsUpdate, dispatcher, result):
         rtime = result.ExpectedTimeOut
         rOID = result.OperationID
         rmt= result.SetMachineType
-        cACK = ccmsUpdate.generateAckCommand(ccmsUpdate.ACKclient, cmdName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+        cACK = ccmsUpdate.generateAckCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
 
     ACKresult = ccmsUpdate.ACKclient.service.UpdateCommandStatus(ctx, cACK)
+
+def handleJoin(ccmsUpdate, result):
+    domain = 'dl.inteleil.com'
+    retry = 1
+
+    commandName = result.CommandName
+
+    if result.CommandParameters != None:
+        nbrparms = len(result.CommandParameters.KeyValueOfstringstring)
+        parmidx = 0
+        while parmidx < nbrparms:
+            pkey = result.CommandParameters.KeyValueOfstringstring[parmidx].Key
+            pval = result.CommandParameters.KeyValueOfstringstring[parmidx].Value
+            if pkey == "Domain Name":
+                domain = pval
+                parmidx = nbrparms + 1
+            else:
+                parmidx += 1
+
+    joinExitCode = ccmsUpdate.dispatcher.join(commandName, domain)
+    ##      ******  in CCMS they are:     *******
+    ##  COMMAND_ISSUED,             == 0
+    ##  COMMAND_RECEIVED,           == 1
+    ##  COMMAND_EXECUTION_STARTED,  == 2
+    ##  COMMAND_EXECUTION_COMPLETE, == 3
+    ##  COMMAND_FAILED,             == 4
+    ##  WAIT_FOR_MANUAL_STEP,       == 5
+    ##  COMMAND_TIMED_OUT,          == 6
+    ##  COMMAND_DELAYED_RESPONSE,   == 7
+    ##  COMMAND_RETRY               == 8
+
+    ## Note: return codes of 0, 2691 (already joined), and AD
+    ## acct create are handled by a bitwise setting on an input
+    ## parm - for join only - with UNjoin you do not have the
+    ## bitwise setting and must check each return
+    ##                                        RC - ver.Nov 2011
+
+    if joinExitCode == 0:
+        rstat = 'COMMAND_EXECUTION_COMPLETE'
+        rsuc = True
+        rresult = 0
+        rerr = result.ErrorCode
+        rtime = result.ExpectedTimeOut
+        rOID = result.OperationID
+        rmt= result.SetMachineType
+        cACK = ccmsUpdate.generateCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+    elif joinExitCode == 1355:
+        # do nothing
+        rstat = 'COMMAND_EXECUTION_COMPLETE'
+        rsuc = True
+        rresult = 0
+        rerr = result.ErrorCode
+        rtime = result.ExpectedTimeOut
+        rOID = result.OperationID
+        rmt= result.SetMachineType
+        cACK = ccmsUpdate.generateCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+    elif joinExitCode == 1219:
+        # Multiple sessions to a server- disconnect all previous
+        # sessions and try again?? will need to add the RETRY
+        # logic from Mahdu TAF client agent
+        rstat = 'COMMAND_RETRY'
+        rsuc = False
+        rresult = 1219
+        rerr = 1219
+        rtime = result.ExpectedTimeOut
+        rOID = result.OperationID
+        rmt= result.SetMachineType
+        cACK = ccmsUpdate.generateCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+
+    # Retry logic from win Client Agent version -
+    # for Join Domain only
+    if retry <= ccmsUpdate.MAX_JOIN_RETRIES:
+        retry =+ 1
+
+        ccmsUpdate.dispatcher.tcpDiag()     ## release renew IP first
+        time.sleep(15)
+
+        joinExitCode = ccmsUpdate.dispatcher.join(commandName, domain)
+        RetryReturn(joinExitCode, rerr, rtime, rOID, rmt)
+    else:
+        rstat = 'COMMAND_FAILED'
+        rsuc = False
+        rresult = None
+        rerr = 'domain join failed'
+        rtime = result.ExpectedTimeOut
+        rOID = result.OperationID
+        rmt= result.SetMachineType
+        cACK = ccmsUpdate.generateCommand(ccmsUpdate.ACKclient, commandName, rstat, rsuc, rresult, rerr, rtime, rOID, rmt)
+
+    # All return codes will now reply back to CCMS via function
+    # below using their own web request - not the original
+    # getcommand request  RC - NOV 2011
+    ACKresult = ccmsUpdate.ACKclient.service.UpdateCommandStatus(ctx, cACK)
+
+    ccmsUpdate.logger.debug('domain join> CCMS return Comand Status Update Result: ' + ACKresult)
 
 # vim:set ai et sts=4 sw=4 tw=80:
