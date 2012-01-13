e04a7c0621708f1bf8eb55e5700dc4a122434742
Sam Hart <hartsn@gmail.com>
2012-01-13 17:13:27 -0500

    need to cd into correct directory

--- uca-bootstrap.py
+++ uca-bootstrap.py
@@ -80,7 +80,7 @@ try:
         exec_command('/etc/init.d/eil_steward.sh stop')
         print 'Linux> Installing dispatcher'
         exec_command('chmod a+x %s/uca/linux/dispatcher/install.sh' % tempDir)
-        exec_command('%s/uca/linux/dispatcher/install.sh' % tempDir)
+        exec_command('cd %s/uca/linux/dispatcher; ./install.sh' % tempDir)
         # FIXME - Missing elevate scipt
         print 'Linux> Starting new client agent'
         exec_command('/etc/init.d/eil_steward.sh start')
