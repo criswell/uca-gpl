82727773926c2a343e2acd6849e3dcd2e775de56
Sam Hart <hartsn@gmail.com>
2012-01-25 14:06:15 -0500

    pull in convenience functions

--- uca_builder.sh
+++ uca_builder.sh
@@ -12,9 +12,65 @@
 
 # FIXME currently this is very hackish
 
+unset LOG_FILE || true
+
 PROGNAME=${0##*/}
 
 MY_CWD=`pwd`
+
+###########################################################
+# Convenience defintions for the various bash color prompts
+###########################################################
+COLOR_RESET='\e[0m'
+COLOR_TRACE='\e[0;34m' # Blue
+COLOR_WARNING='\e[1;33m' # Yellow
+COLOR_ALERT='\e[4;31m' # Underline red
+COLOR_DIE='\e[30m\033[41m' # Red background, black text
+
+#################
+# Trace functions
+#################
+inner_trace () {
+    if [ -n "$LOG_FILE" ]; then
+        echo -e "$(date) $*" >> ${LOG_FILE}
+    else
+        echo -e "$*"
+    fi
+}
+
+warning () {
+    if [ -n "$LOG_FILE" ]; then
+        inner_trace "$*"
+    else
+        inner_trace "${COLOR_WARNING}$*${COLOR_RESET}"
+    fi
+}
+
+trace () {
+    if [ -n "$LOG_FILE" ]; then
+        inner_trace "$*"
+    else
+        inner_trace "${COLOR_TRACE}$*${COLOR_RESET}"
+    fi
+}
+
+alert() {
+    if [ -n "$LOG_FILE" ]; then
+        inner_trace "$*"
+    else
+        inner_trace "${COLOR_ALERT}$*${COLOR_RESET}"
+    fi
+}
+
+die() {
+    if [ -n "$LOG_FILE" ]; then
+        inner_trace "$*"
+    else
+        inner_trace "${COLOR_DIE}$*${COLOR_RESET}"
+    fi
+    cleanup_env
+    exit 1
+}
 
 usage()
 {
@@ -24,7 +80,13 @@ blah blah
 EOF
 }
 
-TMP_REPO=`mktemp -d`
-
+build()
+{
+    trace "!!! Building the unified client agent release"
+    set -x
+    TMP_REPO=`mktemp -d`
+    cd ${MY_CWD}
+    
+}
 
 # vim:set ai et sts=4 sw=4 tw=80:
