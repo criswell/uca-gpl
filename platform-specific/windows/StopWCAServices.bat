b8ae04b46885f5f4e763f4f60a1372b48afed71f
Sam Hart <hartsn@gmail.com>
2012-02-13 12:25:41 -0500

    work on the stop

--- platform-specific/windows/StopWCAServices.bat
+++ platform-specific/windows/StopWCAServices.bat
@@ -2,3 +2,11 @@ REM StopWCAServices.bat
 REM -------------------
 REM Basic tool which stops and removes from the service manager any services
 REM which are associated with the previous Windows Client Agent.
+
+for /F "tokens=3 delims=: " %%H in ('sc query "EILTAFService" ^| findstr "        STATE"') do (
+  if /I "%%H" EQU "RUNNING" (
+   REM Put your code you want to execute here
+   REM For example, the following line
+   net stop "EILTAFService"
+  )
+)
