d23bc447217711690b3a143d0cba7dfe37375678
Sam Hart <hartsn@gmail.com>
2012-02-13 13:19:43 -0500

    start fleshing out handling of the previous services under windows

---
+++ uca-installer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+'''
+uca-installer.py
+----------------
+Basic installer for the UCA. Should be platform agnostic, and run by the
+bootstrapper during installation.
+'''
+
+import os
+
+# Platform determination
+if os.name == 'nt':
+    IS_WINDOWS = True
+    IS_LINUX = False
+    import win32service
+else:
+    IS_WINDOWS = False
+    IS_LINUX = True
+
+def exec_command(cmd):
+    '''
+    given a command, will execute it in the parent environment
+    '''
+    stream = os.popen(cmd)
+    output = stream.readlines()
+    stream.close()
+    for line in output:
+        print line
+
+def win32_stopService(name):
+    '''
+    Given the name of a Windows service, will query for it and attempt to stop
+    it (if running) and delete it.
+    '''
+    try:
+        scm = win32service.OpenSCManager(None, None, win32service.SC_MANAGER_ALL_ACCESS)
+        svc = win32service.OpenService(scm, name, win32service.SC_MANAGER_ALL_ACCESS)
+        status = win32service.QueryServiceStatus(svc)
+        win32service.CloseService(svc)
+        if status[1] == 4:
+            # Service is running, stop it
+            exec_command('net stop %s' % name)
+        exec_command('sc delete %s' % name)
+    except:
+        print "Service %s not found or some other error on access." % name
+
+def win32_stopPreviousServices():
+    '''
+    Attempts to stop and clean up any previous services under Windows.
+    '''
+    win32_stopService('EILTAFService')
+    win32_stopService('EILAutoUpdateService')
+    win32_stopService('EILClientAgent')
+
+
+# vim:set ai et sts=4 sw=4 tw=80:
