b22534c9a5125bb11800d3f5281cef98a8066f51
Sam Hart <hartsn@gmail.com>
2012-02-13 11:47:43 -0500

    add the start of a WCA service stopping batch script

---
+++ platform-specific/windows/StopWCAServices.bat
@@ -0,0 +1,4 @@
+REM StopWCAServices.bat
+REM -------------------
+REM Basic tool which stops and removes from the service manager any services
+REM which are associated with the previous Windows Client Agent.
