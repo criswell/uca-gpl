edbd48fae1f4a6314b3a02d3fdb6b80068fe0126
Sam Hart <hartsn@gmail.com>
2012-03-06 15:47:51 -0500

    usage and some checking

--- uca-release.sh
+++ uca-release.sh
@@ -15,14 +15,36 @@ MY_CWD=`pwd`
 
 . ./uca_helper.sh
 
-
 usage()
 {
     cat <<EOF
-Usage: $PROGNAME /path/to/release
+Usage: $PROGNAME /path/to/release [branch]
 
-The /path/to/release should be an absolute path.
+Parameters:
+    - /path/to/release should be an absolute path to where the release goes.
+
+    - [branch] is an optional named branch for the Mercurial repository.
+
+This release tool should be ran from within a current, working, Mercurial
+repository of the UCA. It will clone the current directory to a temp directory,
+and build the release there.
+
+If your release needs to be built from a specific named branch from the
+repository, then you can give that branch name with the optional parameter
+[branch].
+
 EOF
 }
+
+type hg &> /dev/null || {
+    cat <<EOF
+Mercurial must be installed correctly in order to use this release tool!
+
+Please download an install it from http://mercurial.selenic.com/ if it is not
+available inside your distribution of choice.
+EOF
+}
+
+
 
 # vim:set ai et sts=4 sw=4 tw=80:
