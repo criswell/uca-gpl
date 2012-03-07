bcf12f994e17debaa9274fb87768e4beb985cc7a
Sam Hart <hartsn@gmail.com>
2012-03-07 17:44:59 -0500

    silly testing data

--- src/clientagent/steward/linux_asset.py
+++ src/clientagent/steward/linux_asset.py
@@ -17,12 +17,28 @@ class Linux_Asset(EILAsset):
         This will be called once, when the module is initialized. Put any
         code here that should be called at program start.
         '''
-        raise exceptions.NotImplementedError()
+        pass
 
     def updateAsset(self):
         '''
         Called when we update the asset from getAssetXML
         '''
-        raise exceptions.NotImplementedError()
+        # Complete dummy data to test serialization
+        self.asset['Common']['HostName'] = "scorpious"
+        self.asset['Common']['UUID'] = 'EF38A029-300C-11DE-BA59-0015179E15E4'
+        self.asset['Common']['DomainName'] = 'sh.linuxd.org'
+
+        self.asset['Common']['OS'] = 'Linux'
+        self.asset['Common']['OSVersion'] = 'Kubuntu 11.10'
+        self.asset['Common']['VirtualMachine'] = False
+
+        processor = {
+            'CpuCount' : 4,
+            'CpuModel' : 'Intel',
+            'CoresPerCpu' : 3,
+            'Vt' : True,
+            'VtD' : False,
+        }
+        self.asset['Common']['Processor'] = processor
 
 # vim:set ai et sts=4 sw=4 tw=80:
