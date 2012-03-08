39c65704a11f3654cd12774f6001c47c6172c7da
Sam Hart <hartsn@gmail.com>
2012-03-07 21:28:11 -0500

    attempt to debug why our order is what it is

--- src/clientagent/steward/asset.py
+++ src/clientagent/steward/asset.py
@@ -148,6 +148,7 @@ class EILAsset:
         '''
         if type(obj) == dict:
             for element in obj:
+                print element
                 sub = ET.SubElement(parent, element)
                 if type(obj[element]) == dict or type(obj[element]) == list:
                     self._parseSubElement(obj[element], sub)
