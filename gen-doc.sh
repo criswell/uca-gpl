16b4681f273bce0633260ac50bcb956bf69ee790
Sam Hart <hartsn@gmail.com>
2012-04-23 11:50:00 -0400

    -note on doxygen version

--- gen-doc.sh
+++ gen-doc.sh
@@ -12,6 +12,8 @@ overwrite the current docs/html contents with updated documentation. Use
 caution when running it as cleaning it up if something goes wrong will require
 an 'hg revert'.
 
+Note that this script requires a functional doxygen 1.8.0+ install.
+
 EOF
 
 read -p "Press [ENTER] to continue, or break to cancel : "
