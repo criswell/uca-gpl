550b5e18d704edf92f44da9bd972ce7764441707
Sam Hart <hartsn@gmail.com>
2012-05-08 15:10:31 -0400

    pull in the previous wmi test poc, add allClasses (which are all WMI classes available to python), and add the long-missing DMTF presentation

--- poc/wmi/wmi_test.py
+++ poc/wmi/wmi_test.py
@@ -4,8 +4,54 @@ import wmi
 
 c = wmi.WMI()
 
-# General common stuff
-NTDomain = c.Win32_NTDomain()[-1]
-hostname = NTDomain.Caption
-domain = NTDomain.DomainName
+def hasResult(someObject):
+    if len(someObject) > 0:
+        return someObject[-1]
+    else:
+        return None
 
+# General common stuff
+NTDomain = hasResult(c.Win32_NTDomain())
+if NTDomain:
+    hostname = NTDomain.Caption
+    domain = NTDomain.DomainName
+    joinedToDomain = False
+    if domain:
+        joinedToDomain = True
+
+    print "hostname : %s" % hostname
+    print "domain : %s " % domain
+    print "joinedToDomain: %s" % joinedToDomain
+
+OS = hasResult(c.Win32_OperatingSystem())
+if OS:
+    osName = OS.Caption
+    osVersion = OS.Version
+    mjv = OS.ServicePackMajorVersion
+    mjm = OS.ServicePackMinorVersion
+    osServicePack = "%s.%s" % (mjv, mjm)
+    osArch = OS.OSArchitecture
+
+    BIOS = hasResult(c.Win32_BIOS())
+    if BIOS:
+        biosVersion = BIOS.Version
+    else:
+        biosVersion = None
+
+    print "OS : %s" % osName
+    print "OSVersion : %s" % osVersion
+    print "OSServicePack : %s" % osServicePack
+    print "OSArchitecture : %s" % osArch
+    print "BiosVersion : %s" % biosVersion
+
+# Motherboard
+mobo = hasResult(c.Win32_BaseBoard())
+if mobo:
+    manufacturer = mobo.Manufacturer
+    model = mobo.Model
+    serialNum = mobo.SerialNumber
+    
+    print "Motherboard"
+    print "\t Manufacturer: %s" % manufacturer
+    print "\t Model: %s" % model
+    print "\t SerialNumber: %s" % serialNum
